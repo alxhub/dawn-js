7229d39de79248680403489adbab5fe94f9fc385
sharad-s <sharadshekar@gmail.com>
2019-03-05 17:56:48 -0600

    feat(*): Add cursing to Greeter

---
+++ src/__tests__/Curser.test.ts
@@ -0,0 +1,5 @@
+import { Curser } from '../index';
+
+test('My Greeter', () => {
+  expect(Curser('Carl')).toBe('Curse you Carl');
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 export const Greeter = (name: string) => `Hello ${name}`;
+export const Curser = (name: string) => `Curse you ${name}`;
+
